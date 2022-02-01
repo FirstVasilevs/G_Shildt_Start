fee3a119f5dbdf89e3ace0ce25c59b447e483039
FirstVasilevs <81626051+FirstVasilevs@users.noreply.github.com>
2022-02-01 21:51:17 +0300

    Создал метод main и первую программу

--- src/main/java/Task_01/Example.java
+++ src/main/java/Task_01/Example.java
@@ -8,5 +8,6 @@ public class Example {
     // Выполнение программы на Java начинается с вызова метода main()
     public static void main(String[] args) {
         System.out.println("Java правит Интернетом!");
+        System.out.println();
     }
 }
