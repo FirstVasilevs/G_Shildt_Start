4b5d5ed69289776cb9cb0de1c67915bf8a6647ae
FirstVasilevs <81626051+FirstVasilevs@users.noreply.github.com>
2022-03-31 06:57:37 +0300

    Задание 4, 5 и упражнение 2.1

---
+++ src/main/java/Chapter_02/Exercise_2_1/Sound.java
@@ -0,0 +1,17 @@
+package Chapter_02.Exercise_2_1;
+/*
+Упражнение 2.1
+Вычисление расстояние до места вспышки молнии, звук от которого
+доходит до наблюдателя через 1, 2 секунды.
+*/
+public class Sound {
+
+    public static void main(String[] args) {
+
+        double dist;
+
+        dist = 1100 * 7.2;
+        System.out.println("Растояние до места вспышки молнии " +
+                "составляет " + dist + " футов");
+    }
+}
