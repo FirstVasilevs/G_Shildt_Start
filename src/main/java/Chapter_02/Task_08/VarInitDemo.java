a84c6fe6b7935b4313c3dd756f78a65bab322685
FirstVasilevs <81626051+FirstVasilevs@users.noreply.github.com>
2022-04-01 20:40:53 +0300

    Задание 5 - 9

---
+++ src/main/java/Chapter_02/Task_08/VarInitDemo.java
@@ -0,0 +1,18 @@
+package Chapter_02.Task_08;
+// Демонстрация времени жизни переменной
+public class VarInitDemo {
+
+    public static void main(String[] args) {
+
+        int x;
+
+        for (x = 0; x < 3; x++) {
+            int y = -1; // переменная y инициализируется
+                        //при каждом входе в блок
+            System.out.println("y: " + y); // всегда выводится значение -1
+
+            y = 100;
+            System.out.println("Измененное значение y: " + y);
+        }
+    }
+}
