236f75027e4c59d6298fd5807e0379582b9adc99
FirstVasilevs <81626051+FirstVasilevs@users.noreply.github.com>
2022-04-04 07:00:14 +0300

    Глава 2 вопросы. Глава 3 Задания 1-4

---
+++ src/main/java/Chapter_03/Task_01/KbIn.java
@@ -0,0 +1,21 @@
+package Chapter_03.Task_01;
+// Чтение символа с клавиатуры
+public class KbIn {
+
+    public static void main(String args[])
+
+
+        throws java.io.IOException {
+
+
+            System.out.print("Нажмите нужную клавишу, а затем\n" +
+                    "клавишу ENTER: ");
+
+            char ch = (char) System.in.read();
+
+            System.out.println("Вы нажали клавишу " + ch);
+
+        }
+
+
+}
---
+++ src/main/java/Chapter_03/Task_02/Guess.java
@@ -0,0 +1,17 @@
+package Chapter_03.Task_02;
+// Игра в угадывание букв
+public class Guess {
+
+    public static void main(String args[])
+
+        throws java.io.IOException {
+
+        char ch, answer = 'К';
+        System.out.println("Задумана буква из диапазона A-Z.");
+        System.out.print("Попытайтесь ее угадать: ");
+
+        ch = (char) System.in.read(); // чтение символа с клавиатуры
+        if (ch == answer) System.out.println("** Правильно! **");
+        else System.out.println("...Извините, вы не угадали.");
+    }
+}
