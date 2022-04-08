42fe80234d2ccf3bc7e5d1725fdd3a7105f88896
FirstVasilevs <81626051+FirstVasilevs@users.noreply.github.com>
2022-04-08 22:31:11 +0300

    Глава 3 Задания 14б 15

---
+++ src/main/java/Chapter_03/Task_14/Empty3.java
@@ -0,0 +1,15 @@
+package Chapter_03.Task_14;
+// Тело цикла for может быть пустым
+public class Empty3 {
+
+    public static void main(String args[]) {
+
+        int i;
+        int sum = 0;
+
+        // суммируются числа от 1 до 5
+        for (i = 1; i <= 5; sum += i++);
+
+        System.out.println("Сумма: " + sum);
+    }
+}
