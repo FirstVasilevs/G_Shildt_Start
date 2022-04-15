c29c9e26ea6779a8def605c82f00265395ef258c
FirstVasilevs <81626051+FirstVasilevs@users.noreply.github.com>
2022-04-15 06:55:58 +0300

    Глава 3 Задания 29 упражнение 3.3

--- src/main/java/Chapter_03/Task_29/FindFac.java
+++ src/main/java/Chapter_03/Task_29/FindFac.java
@@ -7,6 +7,7 @@ public class FindFac {
 
     public static void main(String[] args) {
 
+
         for (int i = 2; i <= 100; i++) {
             System.out.print("Делители " + i + ": ");
             for (int j = 2; j < i; j++)
