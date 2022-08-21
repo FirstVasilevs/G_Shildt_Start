782a68e3a249a6522efb8e693ebd688df3faa98d
FirstVasilevs <81626051+FirstVasilevs@users.noreply.github.com>
2022-08-21 06:55:56 +0300

    Глава 5, задания 10 -15

---
+++ src/main/java/Chapter_05/Task_14/StringDemo.java
@@ -0,0 +1,16 @@
+package Chapter_05.Task_14;
+// Знакомство с классом String
+public class StringDemo {
+
+    public static void main(String[] args) {
+
+        // Различные способы объявления строк
+        String str1 = new String("В Java строки - это объекты");
+        String str2 = "Их можно создавать разными способами.";
+        String str3 = new String(str2);
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+    }
+}
